630a26f814e6b53262289d2ad77523b3cf0196ed
Jelena <you@example.com>
2018-06-13 12:57:51 +0200

    Sredjene SO vezane za clana

--- SistemskeOperacije/NadjiClanaSO.cs
+++ SistemskeOperacije/NadjiClanaSO.cs
@@ -19,31 +19,61 @@ namespace SistemskeOperacije
 
                 List<IOpstiDomenskiObjekat> clanovi = GenerickiBroker.Instanca.ExecuteReader(upit, odo);
 
-                foreach (IOpstiDomenskiObjekat o in clanovi)
+                if (clanovi.Count == 0) return false;
+                //foreach (IOpstiDomenskiObjekat o in clanovi)
+                //{
+                Clan c = clanovi[0] as Clan;
+                upit = $"Select * from Zaduzenje where ClanskiBroj = {c.ClanskiBroj}";
+
+                List<IOpstiDomenskiObjekat> listaZaduzenja = GenerickiBroker.Instanca.ExecuteReader(upit, new Zaduzenje());
+
+                foreach (IOpstiDomenskiObjekat zaduzenje in listaZaduzenja)
                 {
-                    Clan c = o as Clan;
-                    upit = $"Select * from (Zaduzenje inner join KnjigaPrimerak on Zaduzenje.PrimerakID = KnjigaPrimerak.PrimerakID) " +
-                    $"inner join Knjiga on KnjigaPrimerak.KnjigaID = Knjiga.KnjigaID where Zaduzenje.ClanskiBroj = {c.ClanskiBroj}";
+                    Zaduzenje z = zaduzenje as Zaduzenje;
+                    z.Clan = c;
 
-                    upit = $"Select * from Zaduzenje where ClanskiBroj = {c.ClanskiBroj}";
+                    upit = $"Select * from KnjigaPrimerak where PrimerakID = {z.KnjigaPrimerak.PrimerakID} and KnjigaID = {z.KnjigaPrimerak.Knjiga.KnjigaID}";
+                    List<IOpstiDomenskiObjekat> listaPrimeraka = GenerickiBroker.Instanca.ExecuteReader(upit, new KnjigaPrimerak());
 
-                    List<IOpstiDomenskiObjekat> listaZaduzenja = GenerickiBroker.Instanca.ExecuteReader(upit, new Zaduzenje());
-
-                    foreach (IOpstiDomenskiObjekat zaduzenje in listaZaduzenja)
+                    foreach (IOpstiDomenskiObjekat prim in listaPrimeraka)
                     {
-                        Zaduzenje z = zaduzenje as Zaduzenje;
-                        z.Clan = c;
+                        KnjigaPrimerak kp = prim as KnjigaPrimerak;
 
-                        upit = $"Select * from KnjigaPrimerak where PrimerakID = {z.KnjigaPrimerak.PrimerakID}";
+                        upit = $"Select * from Knjiga where KnjigaID = {kp.Knjiga.KnjigaID}";
+                        List<IOpstiDomenskiObjekat> knjiga = GenerickiBroker.Instanca.ExecuteReader(upit, kp.Knjiga);
+                        kp.Knjiga = knjiga[0] as Knjiga;
 
-                        c.ListaZaduzenja.Add(z);
+                        upit = $"Select * from KnjigaPrimerak where KnjigaID = {kp.Knjiga.KnjigaID}";
+                        List<IOpstiDomenskiObjekat> listaPrimerakaKnjiga = GenerickiBroker.Instanca.ExecuteReader(upit, new KnjigaPrimerak());
+
+                        foreach (IOpstiDomenskiObjekat lp in listaPrimerakaKnjiga)
+                        {
+                            kp.Knjiga.SpisakPrimeraka.Add(lp as KnjigaPrimerak);
+                        }
+
+                        upit = $"Select * from KnjigaAutor where KnjigaID = {kp.Knjiga.KnjigaID}";
+                        List<IOpstiDomenskiObjekat> listaKnjigaAutor = GenerickiBroker.Instanca.ExecuteReader(upit, new KnjigaAutor());
+
+                        foreach (IOpstiDomenskiObjekat ka in listaKnjigaAutor)
+                        {
+                            KnjigaAutor kaa = ka as KnjigaAutor;
+
+                            upit = $"Select * from Autor where AutorID = {kaa.AutorID}";
+
+                            List<IOpstiDomenskiObjekat> autor = GenerickiBroker.Instanca.ExecuteReader(upit, new Autor());
+                            kp.Knjiga.ListaAutora.Add(autor[0] as Autor);
+                        }
+
+                        z.KnjigaPrimerak = kp;
                     }
 
+                    c.ListaZaduzenja.Add(z);
                 }
-                if (clanovi.Count == 0) return false;
 
-                Rezultat = clanovi[0];
-                
+            //}
+
+                Rezultat = c;
+
                 return true;
             }
             catch (Exception e)
